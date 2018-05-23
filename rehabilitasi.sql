603e28d7a8eceb62868553ce7e3e2a429ca61f21
DESKTOP-EQEHUMC\Shafly <shaflynaufal96@gmail.com>
2018-05-23 12:40:02 +0700

    tes

--- rehabilitasi.sql
+++ rehabilitasi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 04, 2018 at 02:01 PM
+-- Generation Time: May 23, 2018 at 07:38 AM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
@@ -25,15 +25,52 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `gerak`
+--
+
+CREATE TABLE `gerak` (
+  `id` int(11) NOT NULL,
+  `nama_gerak` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `gerak`
+--
+
+INSERT INTO `gerak` (`id`, `nama_gerak`) VALUES
+(1, 'Menekan tombol'),
+(2, 'Memegang benda'),
+(3, 'Mengangkat benda'),
+(4, 'Mengayunkan benda'),
+(5, 'Memencet remote tv'),
+(6, 'Makan dengan sendok'),
+(7, 'Memilih barang di rak'),
+(8, 'Minum dari gelas'),
+(9, 'Memutar kran air'),
+(10, 'Membuka ganggang pintu'),
+(11, 'Menyikat gigi');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `report`
 --
 
 CREATE TABLE `report` (
   `id` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
-  `tanggal` varchar(50) NOT NULL,
+  `tanggal` datetime DEFAULT CURRENT_TIMESTAMP,
+  `gerak` int(11) NOT NULL,
   `value` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `report`
+--
+
+INSERT INTO `report` (`id`, `id_user`, `tanggal`, `gerak`, `value`) VALUES
+(1, 1, '2018-05-18 10:08:46', 1, 7),
+(2, 1, '2018-05-18 10:08:47', 1, 9);
 
 -- --------------------------------------------------------
 
@@ -43,47 +80,34 @@ CREATE TABLE `report` (
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
-  `user` varchar(50) NOT NULL,
-  `gerak1` int(11) NOT NULL DEFAULT '0',
-  `gerak2` int(11) NOT NULL DEFAULT '0',
-  `gerak3` int(11) NOT NULL DEFAULT '0',
-  `gerak4` int(11) NOT NULL DEFAULT '0',
-  `gerak5` int(11) NOT NULL DEFAULT '0',
-  `gerak6` int(11) NOT NULL DEFAULT '0',
-  `gerak7` int(11) NOT NULL DEFAULT '0',
-  `gerak8` int(11) NOT NULL DEFAULT '0',
-  `gerak9` int(11) NOT NULL DEFAULT '0',
-  `gerak10` int(11) NOT NULL DEFAULT '0',
-  `gerak11` int(11) NOT NULL DEFAULT '0'
+  `user` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `user`, `gerak1`, `gerak2`, `gerak3`, `gerak4`, `gerak5`, `gerak6`, `gerak7`, `gerak8`, `gerak9`, `gerak10`, `gerak11`) VALUES
-(1, 'Bambang', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(2, 'Winda', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(3, 'Didi', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(4, 'Nisa', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(5, 'Dimas', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(6, 'Dedi', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(7, 'Santri', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(8, 'Aqua', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(9, 'tes', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(12, 'hehe', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(13, 'coba tes', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO `user` (`id`, `user`) VALUES
+(1, 'Bambang'),
+(2, 'Winda');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `gerak`
+--
+ALTER TABLE `gerak`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `report`
 --
 ALTER TABLE `report`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `user` (`id_user`);
+  ADD KEY `user` (`id_user`),
+  ADD KEY `gerak` (`gerak`);
 
 --
 -- Indexes for table `user`
@@ -96,16 +120,22 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `gerak`
+--
+ALTER TABLE `gerak`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
 -- AUTO_INCREMENT for table `report`
 --
 ALTER TABLE `report`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -115,6 +145,7 @@ ALTER TABLE `user`
 -- Constraints for table `report`
 --
 ALTER TABLE `report`
+  ADD CONSTRAINT `report_ibfk_1` FOREIGN KEY (`gerak`) REFERENCES `gerak` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
